13eeb684bf4b19f1afd18f414665059e1bb12b30
mgunn-goldfish <mgunn@goldfishai.net>
2023-05-20 21:34:20 -0500

    Questions and answers filled from state dictionary, will pull out to separate file now. Backup for that

--- src/App.js
+++ src/App.js
@@ -6,12 +6,46 @@ import goldfishLogo from './images/logo.png';
 import profilePic from './images/profile.png';
 import './App.css';
 
+function Answer({ answerID, answer, questionID, setQuestionsAnswered }) {
+  const [selected, setSelected] = useState(false);
+
+  const handleAnswerClick = () => {
+    setSelected(!selected);
+
+    setQuestionsAnswered((prevAnswers) => {
+      const newAnswer = { questionID: questionID, answerID: [answerID] };
+      if (selected) {
+        // remove the answer if it is already selected
+        return prevAnswers.filter(
+          (answer) =>
+            answer.questionID !== questionID || answer.answerID !== answerID
+        );
+      } else {
+        // add the new answer
+        return [...prevAnswers, newAnswer];
+      }
+    });
+  };
+
+  return (
+    <button
+      className={`answerButton ${selected ? "selected" : ""}`}
+      onClick={handleAnswerClick}
+    >
+      {answer}
+    </button>
+  );
+}
+
 function Home() {
   // document.getElementById("HomeLink").style.display = "none";
   const [isDropdownOpen, setDropdownOpen] = useState(false);
   const dropdownRef = useRef(null);
 
-  const toggleDropdown = () => {
+  const toggleDropdown = (event) => {
+    // if (isDropdownOpen && (dropdownRef.current === event.target || dropdownRef.current.contains(event.target))) {
+    //   return;
+    // }
     setDropdownOpen(!isDropdownOpen);
   };
 
@@ -19,14 +53,21 @@ function Home() {
     if (dropdownRef.current && !dropdownRef.current.contains(event.target)) {
       setDropdownOpen(false);
     }
-  };
+};
 
-  useEffect(() => {
-    document.addEventListener('mousedown', handleClickOutside);
-    return () => {
-      document.removeEventListener('mousedown', handleClickOutside);
+useEffect(() => {
+    const handleWindowClick = (event) => {
+        if (dropdownRef.current && !dropdownRef.current.contains(event.target)) {
+            handleClickOutside(event);
+        }
     };
-  }, []);
+
+    window.addEventListener("mousedown", handleWindowClick);
+    return () => {
+        window.removeEventListener("mousedown", handleWindowClick);
+    };
+}, []);
+
   return (
     <div className="Home">
       <header className="Banner">
@@ -48,12 +89,13 @@ function Home() {
             {/* </nav> */}
           </div>
         </div>
-        <div className="ProfileDropdown">
+        <div className="ProfileDropdown" ref={dropdownRef}>
           <button className="ProfileButton" onClick={toggleDropdown}>
             <img src={profilePic} alt="Profile" className="ProfileIcon" />
           </button>
           {isDropdownOpen && (
-            <div className="DropdownContent" ref={dropdownRef}>
+            // <div className="DropdownContent" ref={dropdownRef}>
+            <div className="DropdownContent">
               <ul className="DropdownMenu">
                 <button>Profile</button>
                 <button>Settings</button>
@@ -114,8 +156,76 @@ function EmployerPage() {
   const [isDropdownOpen, setDropdownOpen] = useState(false);
   const dropdownRef = useRef(null);
   const [searchTerm, setSearchTerm] = useState('');
+  const [questions, setQuestions] = useState([
+    {
+      questionID: 1,
+      category: "Category1",
+      tags: ["tag1", "tag2"],
+      question: "How would you describe your level of experience with X?",
+      answers: [
+        {answerID: 1, answer: "Answer1"},
+        {answerID: 2, answer: "Answer2"},
+        {answerID: 3, answer: "Answer3"},
+        {answerID: 4, answer: "Answer4"}
+      ]
+    },
+    {
+      questionID: 2,
+      category: "Category2",
+      tags: ["tag3", "tag4"],
+      question: "What are your thoughts on A?",
+      answers: [
+        {answerID: 1, answer: "Answer1"},
+        {answerID: 2, answer: "Answer2"},
+        {answerID: 3, answer: "Answer3"},
+        {answerID: 4, answer: "Answer4"}
+      ]
+    },
+    {
+      questionID: 3,
+      category: "Category3",
+      tags: ["tag3", "tag4"],
+      question: "What are your thoughts on B?",
+      answers: [
+        {answerID: 1, answer: "Answer1"},
+        {answerID: 2, answer: "Answer2"},
+        {answerID: 3, answer: "Answer3"},
+        {answerID: 4, answer: "Answer4"}
+      ]
+    },
+    {
+      questionID: 4,
+      category: "Category4",
+      tags: ["tag2", "tag3"],
+      question: "What are your thoughts on C?",
+      answers: [
+        {answerID: 1, answer: "Answer1"},
+        {answerID: 2, answer: "Answer2"},
+        {answerID: 3, answer: "Answer3"},
+        {answerID: 4, answer: "Answer4"}
+      ]
+    },
+    {
+      questionID: 5,
+      category: "Category1",
+      tags: ["tag3", "tag5"],
+      question: "What are your thoughts on D?",
+      answers: [
+        {answerID: 1, answer: "Answer1"},
+        {answerID: 2, answer: "Answer2"},
+        {answerID: 3, answer: "Answer3"},
+        {answerID: 4, answer: "Answer4"}
+      ]
+    },
+  ]);
 
-  const toggleDropdown = () => {
+  const [questionsAnswered, setQuestionsAnswered] = useState([]);
+  
+
+  const toggleDropdown = (event) => {
+    // if (isDropdownOpen && (dropdownRef.current === event.target || dropdownRef.current.contains(event.target))) {
+    //   return;
+    // }
     setDropdownOpen(!isDropdownOpen);
   };
 
@@ -126,9 +236,15 @@ function EmployerPage() {
   };
 
   useEffect(() => {
-    document.addEventListener('mousedown', handleClickOutside);
+    const handleWindowClick = (event) => {
+        if (dropdownRef.current && !dropdownRef.current.contains(event.target)) {
+            handleClickOutside(event);
+        }
+    };
+
+    window.addEventListener("mousedown", handleWindowClick);
     return () => {
-      document.removeEventListener('mousedown', handleClickOutside);
+        window.removeEventListener("mousedown", handleWindowClick);
     };
   }, []);
 
@@ -191,39 +307,25 @@ function EmployerPage() {
               <button className="AddButton">Add</button>
             </div>
             <div className="ScrollableContent">
-              <Collapsible trigger="How would you describe your level of experience with X?" open={true}>
-                <div className="answerSection">
+              {questions.map((question) => (
+                <Collapsible trigger={question.question}>
+                  <div className="answerSection">
                   <hr />
-                  <div className="GridOfButtons">
-                    <button>Choice 1</button>
-                    <button>Choice 2</button>
-                    <button>Choice 3</button>
-                    <button>Choice 4</button>
+                    <div className="GridOfButtons">
+                    {question.answers.map((answer) => (
+                      <Answer
+                        key={answer.answerID}
+                        answerID={answer.answerID}
+                        answer={answer.answer}
+                        questionID={question.questionID}
+                        setQuestionsAnswered={setQuestionsAnswered}
+                      />
+                    ))}
+                    </div>
                   </div>
-                </div>
-              </Collapsible>
-              <Collapsible trigger="Another Question">
-                <div className="answerSection">
-                  <hr />
-                  <div className="GridOfButtons">
-                    <button>Choice 1</button>
-                    <button>Choice 2</button>
-                    <button>Choice 3</button>
-                    <button>Choice 4</button>
-                  </div>
-                </div>
-              </Collapsible>
-              <Collapsible trigger="Third Question">
-                <div className="answerSection">
-                  <hr />
-                  <div className="GridOfButtons">
-                    <button>Choice 1</button>
-                    <button>Choice 2</button>
-                    <button>Choice 3</button>
-                    <button>Choice 4</button>
-                  </div>
-                </div>
-              </Collapsible>
+
+                </Collapsible>
+              ))}
             </div>
           </div>
 
@@ -267,6 +369,7 @@ function EmployerPage() {
                 </div>
               </Collapsible>
             </div>
+            <button className="saveButton">Save</button>
           </div>
         </div>
       </div>
