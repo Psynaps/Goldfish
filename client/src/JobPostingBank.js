04940c2667da500ec61434033d01ccda2dc7ac6a
mgunn-goldfish <mgunn@goldfishai.net>
2023-09-27 23:57:31 -0500

    Fix border not being rounded in important and nice to have boxes on job posting bank

--- client/src/JobPostingBank.js
+++ client/src/JobPostingBank.js
@@ -9,7 +9,7 @@ const JobPostingBank = ({ jobPostingQuestions, onQuestionSelect, onAnswerSelect 
     return (
         <VStack spacing={4} align='stretch' maxHeight='50vh' overflowY='auto' w='100%'>
             <Text mt={0} fontSize='lg' fontFamily='Inter' textDecoration='underline'>Important</Text>
-            <VStack spacing={4} align='stretch' w='100%' p={3} bg='blue.600' borderColor='orange.600' borderWidth='3px' borderStyle='dashed'>
+            <VStack spacing={4} align='stretch' w='100%' p={3} bg='blue.600' borderColor='orange.600' borderWidth='3px' borderStyle='dashed' borderRadius='xl'>
 
                 {jobPostingQuestions.filter(q => q.importance === 2).map((question, index) => (
                     <Box key={question.questionID} bg='blue.400' borderRadius='2xl' _hover={{ bg: "blue.500" }}>
@@ -48,7 +48,7 @@ const JobPostingBank = ({ jobPostingQuestions, onQuestionSelect, onAnswerSelect 
             </VStack>
 
             <Text mt={6} mb={2} fontSize='lg' fontFamily='Inter' textDecoration='underline'>Nice to Have</Text>
-            <VStack spacing={4} align='stretch' w='100%' p={3} bg='blue.600' borderColor='orange.600' borderWidth='3px' borderStyle='dashed'>
+            <VStack spacing={4} align='stretch' w='100%' p={3} bg='blue.600' borderColor='orange.600' borderWidth='3px' borderStyle='dashed' borderRadius='xl'>
                 {jobPostingQuestions.filter(q => q.importance === 1).map((question, index) => (
                     <Box key={question.questionID} bg='blue.400' borderRadius='2xl'>
                         <Question
