128d9869b0573a886a2190d9f698ba92fcb9fd1f
mgunn-goldfish <mgunn@goldfishai.net>
2024-02-25 12:21:47 -0600

    Fix Database String env var

--- server/server.js
+++ server/server.js
@@ -35,14 +35,14 @@ app.use(express.static(path.resolve(__dirname, '../client/build'))); // Maybe ad
 const pool = (() => {
     if (process.env.NODE_ENV !== 'production') {
         return new Pool({
-            connectionString: process.env.DATABASE_STRING,
+            connectionString: process.env.DATABASE_URL,
             ssl: {
                 rejectUnauthorized: false
             }
         });
     } else {
         return new Pool({
-            connectionString: process.env.DATABASE_STRING,
+            connectionString: process.env.DATABASE_URL,
             ssl: {
                 rejectUnauthorized: false
             }
